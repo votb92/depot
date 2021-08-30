36e10989712a4dbce42167d49b80e07b47e1d228
votb92 <thinhvo1101@gmail.com>
2021-08-30 14:28:49 -0400

    adding feeds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,6 +61,18 @@ class ProductsController < ApplicationController
     end
   end
 
+
+  # Rails look for a template named who_bought.atom.builder
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last 
+    if stale?(@latest_order)
+      respond_to do |format| 
+        format.atom
+      end 
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
+  resources :products do
+    get :who_bought, on: :member
+  end
   #get 'store/index'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
